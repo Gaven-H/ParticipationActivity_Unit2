18687d2e6cbdad788da131d5583b0101adb95bf1
Bemst <gahuforschool@gmail.com>
2026-01-21 14:08:41 -0500

    Formatting Cleanup (not inserting into string)

--- Submission.py
+++ Submission.py
@@ -2,16 +2,16 @@ name = "Gaven H"
 
 name_with_spaces = "     " + name + "     "
 
-print(f"{name_with_spaces.lower()}")
+print(name_with_spaces.lower())
 
-print(f"{name_with_spaces.upper()}")
+print(name_with_spaces.upper())
 
-print(f"{name_with_spaces.title()}")
+print(name_with_spaces.title())
 
 width = 30
 
-print(f"{name_with_spaces.ljust(width, '_')}")
+print(name_with_spaces.ljust(width, '_'))
 
-print(f"{name_with_spaces.rjust(width, '_')}")
+print(name_with_spaces.rjust(width, '_'))
 
-print(f"{name_with_spaces.center(width, '_')}")
+print(name_with_spaces.center(width, '_'))
