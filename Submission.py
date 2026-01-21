ea85cf8360de6d8217cc9464778101cae50a7ecb
Bemst <gahuforschool@gmail.com>
2026-01-21 13:56:28 -0500

    Print with Alignment

--- Submission.py
+++ Submission.py
@@ -8,3 +8,10 @@ print(f"{name_with_spaces.upper()}")
 
 print(f"{name_with_spaces.title()}")
 
+width = 15
+
+print(f"{name_with_spaces.center(width, "_").ljust(width, "_")}")
+
+print(f"{name_with_spaces.rjust(width, "_")}")
+
+print(f"{name_with_spaces.center(width, "_")}")
