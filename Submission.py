522fcdbe3920e7696d7e2c51dc05e6f7eb9c8cba
Bemst <gahuforschool@gmail.com>
2026-01-21 13:51:44 -0500

    Print in Cases (Lower, Upper, Title)

--- Submission.py
+++ Submission.py
@@ -2,6 +2,9 @@ name = "Gaven H"
 
 name_with_spaces = "     " + name + "     "
 
+print(f"{name_with_spaces.lower()}")
 
+print(f"{name_with_spaces.upper()}")
 
+print(f"{name_with_spaces.title()}")
 
